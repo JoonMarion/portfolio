90a06a3f0be84ff25f4923ed6c60880697e73382
JoonMarion <marianojoao52@gmail.com>
2022-11-16 17:24:38 -0300

    fix: add project

--- src/components/mainProjects/MainProjects.jsx
+++ src/components/mainProjects/MainProjects.jsx
@@ -3,9 +3,10 @@ import './MainProjects.css';
 import ScrollContainer from 'react-indiana-drag-scroll';
 import DevQuiz from '../../assets/dev-quiz.jpeg';
 import FinanceManage from '../../assets/finance-manage.jpeg';
-import Pokedex from '../../assets/pokedex.jpeg';
+import Pokedex from '../../assets/pokedex.png';
 import Calculator from '../../assets/calculator.jpeg';
 import Project from '../project/Project';
+import InstagramLogin from '../../assets/instagram-login-clone.png';
 
 const MainProjects = () => {
     return (
@@ -39,6 +40,12 @@ const MainProjects = () => {
                         img={Pokedex}
                         description="Pokedex with Vanilla JavaScript"
                         link="https://joonmarion.github.io/pokedex/"
+                    />
+                    <Project
+                        title="Instagram Login Clone"
+                        img={InstagramLogin}
+                        description="Instagram login screen clone"
+                        link="https://joonmarion.github.io/login-instagram/"
                     />
                     <Project
                         title="Calculator"
