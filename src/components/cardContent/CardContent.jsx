7e87e051f045013f8819459c12b8d9885ca371f1
JoonMarion <marianojoao52@gmail.com>
2022-11-17 00:24:32 -0300

    fix: main content text

--- src/components/cardContent/CardContent.jsx
+++ src/components/cardContent/CardContent.jsx
@@ -7,18 +7,19 @@ const CardContent = () => {
         <div className="card">
             <div className="avatar-card secondary">
                 <div id="divContent" className="content">
-                    Hey guys, my name is João Mariano. I'm from Castanhal, Pará. I was born and raised here. I'm
-                    currently an intern on the development team at Sea Telecom and a Software Engineering student at the
-                    Universidade do Estado do Pará. Since i was a kid, i've been an enthusiast of everything that
+                    &emsp;&emsp;Hey guys, my name is João Mariano. I'm from Castanhal, Pará. I was born and raised here.
+                    I'm currently an intern on the development team at Sea Telecom and a Software Engineering student at
+                    the Universidade do Estado do Pará. Since i was a kid, i've been an enthusiast of everything that
                     involves technology, from hardware to software, and today, working as a developer, i'm completely
-                    sure of my destiny and my future in the development career. I love to code! I developed several
-                    management systems in the company where I work, most of them in Django, I created docker images and
-                    they are all in containers, on the company's server. Creating a small microservices ecosystem. I had
-                    experience with agile development. I took several courses of personal and professional improvement.
-                    Among the technologies I had more contact with are React JS, Django, JavaScript, Python, HTML and
-                    CSS.{' '}
+                    sure of my destiny and my future in the development career. I love to code!
+                    <br /> <br />
+                    &emsp;&emsp;I developed several management systems in the company where I work, most of them in
+                    Django, I created docker images and they are all in containers, on the company's server. Creating a
+                    small microservices ecosystem. I had experience with agile development. I took several courses of
+                    personal and professional improvement. Among the technologies i had more contact with are React JS,
+                    Django, JavaScript, Python, HTML and CSS.{' '}
                     <a href="/" className="link-hover">
-                        Lean more...
+                        Read more...
                     </a>
                 </div>
             </div>
