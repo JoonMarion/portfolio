072c2f7da502caa3678296b5341f913be44a4735
JoonMarion <marianojoao52@gmail.com>
2022-12-09 03:18:42 -0300

    fix: telegram

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/style-prop-object */
 import React from 'react';
 import './Contacts.css';
+import { BiMailSend } from 'react-icons/bi';
+import SocialNetworks from './../socialNetworks/SocialNetworks';
 
 const Contacts = () => {
     const Contacts = '<Contatos />';
@@ -12,7 +15,7 @@ const Contacts = () => {
                 <div className="contacts">
                     <div>
                         <h2>Telefone</h2>
-                        <p>91 988432057</p>
+                        <p>(91) 98843-2057</p>
                     </div>
                     <div>
                         <h2>Email</h2>
@@ -20,37 +23,52 @@ const Contacts = () => {
                     </div>
                     <div>
                         <h2>Telegram</h2>
-                        <a href="t.me/JoonMarion">t.me/joonmarion</a>
+                        <a href="https://t.me/JoonMarion" className="link-hover">
+                            t.me/joonmarion
+                        </a>
                     </div>
                     <div>
                         <h2>Localização</h2>
                         <p>Castanhal - PA</p>
                     </div>
+                    <div className="contacts-social-networks">
+                        <SocialNetworks size="25" />
+                    </div>
                 </div>
                 <div className="send-email">
                     <h2 className="send-email-title">Me envie um email</h2>
-                    <form className="form" onSubmit={() => {}}>
+                    <form action="https://formsubmit.co/marianojoao52@gmail.com" className="form" method="POST">
                         <ul className="forms">
                             <li className="form-content">
                                 <label for="name" className="form-label secondary">
                                     Digite seu nome
                                 </label>
-                                <input className="input form-style" type="text" name="name" />
+                                <input className="input form-style" type="text" name="name" required />
                             </li>
                             <li className="form-content">
                                 <label for="email" className="form-label secondary">
                                     Digite um email válido
                                 </label>
-                                <input className="input form-style" type="text" name="email" />
+                                <input className="input form-style" type="text" name="email" required />
+                            </li>
+                            <li className="form-content">
+                                <label for="message" className="form-label secondary">
+                                    Digite o título da mensagem
+                                </label>
+                                <input type="text" name="_subject" className="input form-style" required />
                             </li>
                             <li className="form-content">
                                 <label for="message" className="form-label secondary">
                                     Digite sua mensagem
                                 </label>
-                                <textarea className="textarea form-style" name="message" />
+                                <textarea className="textarea form-style" name="message" required />
                             </li>
                             <li className="form-content form-button">
-                                <input className="button form-style secondary" type="submit" value="Enviar" />
+                                <button className="button form-style secondary centered" type="submit">
+                                    {' '}
+                                    Enviar
+                                    <BiMailSend size={20} className="margin-icon secondary" />
+                                </button>
                             </li>
                         </ul>
                     </form>
