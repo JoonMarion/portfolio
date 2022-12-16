d85de562e11a858a79352aa1fad7bb35d019a612
JoonMarion <marianojoao52@gmail.com>
2022-12-16 03:45:01 -0300

    fix: final version

--- src/components/cardContent/CardContent.jsx
+++ src/components/cardContent/CardContent.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable jsx-a11y/no-redundant-roles */
 // import MiniCard from '../miniCard/MiniCard';
 import './CardContent.css';
 import Modal from '../modal/Modal';
 import SocialNetworks from '../socialNetworks/SocialNetworks';
 import { BsFillArrowDownSquareFill } from 'react-icons/bs';
+import { MdOpenInNew } from 'react-icons/md';
 
 const CardContent = () => {
     return (
@@ -15,6 +17,22 @@ const CardContent = () => {
                         <h2 className="first-about secondary">Engenharia de Software</h2>
                         <h2 className="first-about secondary">Entusiasta de tecnologia e ciências exatas</h2>
                         <Modal />
+
+                        <div className="button-43-wrapper">
+                            <a
+                                href="https://resume.io/r/CXkGXKu00"
+                                className="curriculo-link"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <button className="button-curriculo" role="button">
+                                    <p className="button-43-content">
+                                        Currículo <MdOpenInNew className="icon margin-icon" size={18} />
+                                    </p>
+                                </button>
+                            </a>
+                        </div>
+
                         <div className="social-networks-wrapper">
                             <SocialNetworks size="25" style={{ justifyContent: 'space-evenly' }} />
                         </div>
