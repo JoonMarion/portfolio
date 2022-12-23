689c521d407b4297e98b686d1e157fbf49b638a4
JoonMarion <marianojoao52@gmail.com>
2022-12-23 04:11:24 -0300

    fix: background social network

--- src/components/socialNetworks/SocialNetworks.jsx
+++ src/components/socialNetworks/SocialNetworks.jsx
@@ -7,31 +7,31 @@ import { AiTwotoneMail } from 'react-icons/ai';
 const SocialNetworks = ({ size, style }) => {
     return (
         <div className="networks-wrapper" style={style}>
-            <div className="social-icons">
-                <a href="https://www.linkedin.com/in/joaomariano52/" target="_blank" rel="noreferrer">
+            <a href="https://www.linkedin.com/in/joaomariano52/" target="_blank" rel="noreferrer">
+                <div className="social-icons centered">
                     <FaLinkedin size={size} className="social-icon secondary" />
-                </a>
-            </div>
-            <div className="social-icons">
-                <a href="https://github.com/JoonMarion" target="_blank" rel="noreferrer">
+                </div>
+            </a>
+            <a href="https://github.com/JoonMarion" target="_blank" rel="noreferrer">
+                <div className="social-icons centered">
                     <FaGithub size={size} className="social-icon secondary" />
-                </a>
-            </div>
-            <div className="social-icons">
-                <a href="https://www.instagram.com/joonmarion/" target="_blank" rel="noreferrer">
+                </div>
+            </a>
+            <a href="https://www.instagram.com/joonmarion/" target="_blank" rel="noreferrer">
+                <div className="social-icons centered">
                     <BsInstagram size={size} className="social-icon secondary" />
-                </a>
-            </div>
-            <div className="social-icons">
-                <a
-                    href="https://mail.google.com/mail/u/0/?fs=1&to=marianojoao52@gmail.com&su=Ol%C3%A1,+Jo%C3%A3o!&body=Vamos+conversar!&tf=cm"
-                    target="_blank"
-                    rel="noreferrer"
-                    className="social-tag-a"
-                >
+                </div>
+            </a>
+            <a
+                href="https://mail.google.com/mail/u/0/?fs=1&to=marianojoao52@gmail.com&su=Ol%C3%A1,+Jo%C3%A3o!&body=Vamos+conversar!&tf=cm"
+                target="_blank"
+                rel="noreferrer"
+                className="social-tag-a"
+            >
+                <div className="social-icons centered">
                     <AiTwotoneMail size={size} className="social-icon secondary" />
-                </a>
-            </div>
+                </div>
+            </a>
         </div>
     );
 };
